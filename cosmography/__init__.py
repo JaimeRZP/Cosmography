27795e48f63cbbbd4afc6e9aba540cee90979f0b
Jaime RZ <jaimerz011235813@gmail.com>
2024-04-09 16:06:36 +0100

    Update __init__.py

--- cosmography/__init__.py
+++ cosmography/__init__.py
@@ -1,12 +1,12 @@
 # flake8: noqa
 from .base import CosmographyBase
 from .sixdf import Sixdf
-from .BOSS import BOSS
+from .BOSSDR12 import BOSSDR12
 from .CC import CC
 from .CMB import CMB
 from .pantheonDS17 import PantheonDS17
 from .DSS import DSS
-from .eBOSS import eBOSS
+from .eBOSSDR16 import eBOSSDR16
 from .fastsound import FastSound
 from .vipers import Vipers
 from .wigglez import Wigglez
